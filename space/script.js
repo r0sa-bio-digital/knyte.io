4f62ea9ef43ff31796c49851d66f2e5fb725cd2b
Glitch (knyte-io) <none>
2020-03-29 17:27:12 +0000

    visual theme added

--- space/script.js
+++ space/script.js
@@ -1,21 +1,46 @@
 let svgNameSpace;
+const visualTheme = {
+  canvas: {
+    color: '#160f19',
+  },
+  rect: {
+    strokeColor: '#ffffdd',
+    strokeWidth: '4',
+    fillColor: '#ffc0cb',
+    defaultWidth: 32,
+    defaultHeight: 32,
+  }
+};
+
+function addRect(canvasElement, position)
+{
+  const w = visualTheme.rect.defaultHeight;
+  const h = visualTheme.rect.defaultWidth;
+  const x = position.x - w/2;
+  const y = position.y - h/2;
+  const rect = document.createElementNS(svgNameSpace, 'rect');
+  rect.setAttribute('x', x);
+  rect.setAttribute('y', y);
+  rect.setAttribute('width', w);
+  rect.setAttribute('height', h);
+  rect.setAttribute('fill', visualTheme.rect.fillColor);
+  rect.setAttribute('stroke', visualTheme.rect.strokeColor);
+  rect.setAttribute('stroke-width', visualTheme.rect.strokeWidth);
+  canvasElement.appendChild(rect);
+}
 
 function onClickVisualRoot(e)
 {
-  const visualRoot = document.getElementById('visualRoot');
-  const rect = document.createElementNS(svgNameSpace, 'rect');
-  rect.setAttribute('x', e.offsetX);
-  rect.setAttribute('y', e.offsetY);
-  rect.setAttribute('height', '16');
-  rect.setAttribute('width', '16');
-  rect.setAttribute('fill', 'aqua');
-  rect.setAttribute('stroke', 'blue');
-  visualRoot.appendChild(rect);
+  addRect(
+    document.getElementById('visualRoot'),
+    {x: e.offsetX, y: e.offsetY}
+  );
 }
 
 function onLoadBody(e)
 {
   const visualRoot = document.getElementById('visualRoot');
+  visualRoot.style.backgroundColor = visualTheme.canvas.color;
   svgNameSpace = visualRoot.getAttribute('xmlns');
   visualRoot.addEventListener('click', onClickVisualRoot, false);
   console.log('ready');
