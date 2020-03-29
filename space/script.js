7dee3a5d06f486878361758abf3ec7b562026348
Glitch (knyte-io) <none>
2020-03-29 17:05:08 +0000

    add rect to visual root by click

--- space/script.js
+++ space/script.js
@@ -1,4 +1,22 @@
+let svgNameSpace;
+
+function onClickVisualRoot(e)
+{
+  const visualRoot = document.getElementById('visualRoot');
+  const rect = document.createElementNS(svgNameSpace, 'rect');
+  rect.setAttribute('x', e.offsetX);
+  rect.setAttribute('y', e.offsetY);
+  rect.setAttribute('height', '16');
+  rect.setAttribute('width', '16');
+  rect.setAttribute('fill', 'aqua');
+  rect.setAttribute('stroke', 'blue');
+  visualRoot.appendChild(rect);
+}
+
 function onLoadBody(e)
 {
+  const visualRoot = document.getElementById('visualRoot');
+  svgNameSpace = visualRoot.getAttribute('xmlns');
+  visualRoot.addEventListener('click', onClickVisualRoot, false);
   console.log('ready');
 }
