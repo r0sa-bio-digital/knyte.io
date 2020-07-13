65d698a2788ccdee2b4895dd95bde8d3592e7ac9
x-rodionov <code512edu@gmail.com>
2020-07-14 03:49:27 +0600

    reparse body

--- functions/runblock.js
+++ functions/runblock.js
@@ -848,6 +848,6 @@ exports.handler = async (event, context) => {
   const body = JSON.parse(event.body);
   return {
     statusCode: 200,
-    body: JSON.stringify({body: event.body, server: true})
+    body: JSON.stringify({body, server: true})
   };
 };
