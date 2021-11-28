b16fbfa3ba1309821823bcc36c3ee2f605ad1caf
x-rodionov <code512edu@gmail.com>
2021-11-28 09:30:16 +0600

    test globals 4

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,70 @@
 const a = {b: 'c'};
 
+function onComplete(success)
+{
+  let result = JSON.stringify({success: false, result: 'run block execution failed.'});
+  if (success)
+  {
+    result = JSON.stringify({success: true, result: 'OK'});
+  }
+  console.log('run block result: ' + result);
+  resolve(result);
+}
+
+function runBlockHandleClick(codeText, resolve)
+{
+  let runComplete = false;
+  try
+  {
+    const AsyncFunction = Object.getPrototypeOf(async function(){}).constructor;
+    const evalText = 'new AsyncFunction(useStrict + codeText)';
+    const codeFunction = eval(evalText);
+    setTimeout(
+      function()
+      {
+        let promiseComplete = false;
+        try
+        {
+          let codeComplete = false;
+          const promiseResults = eval('codeFunction(' + actualParametersList + ')');
+          promiseResults.then(
+            function(results)
+            {
+            }
+          ).finally(
+            function()
+            {
+              onComplete(codeComplete);
+            }
+          );
+          promiseComplete = true;
+        }
+        finally
+        {
+          if (!promiseComplete)
+          {
+            onComplete(false);
+          }
+        }
+      }, 
+      runBlockDelay
+    );
+    runComplete = true;
+  }
+  finally
+  {
+  }
+}
+
+function runBlockAsync(body)
+{
+  return new Promise(
+    (resolve) => {runBlockHandleClick(codeText, resolve);}
+  );
+}
+
 exports.handler = async (event, context) => {
-  setTimeout(() => eval('"use strict";\nconsole.log(a);'), 10);
+  console.log(await runBlockAsync('"use strict";\nconsole.log(a);'));
   return {
     statusCode: 200,
     body: 'hello from netlify functions'
