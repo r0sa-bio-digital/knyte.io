e1fac6bec20125b411b75caa7d717e4469a48c2a
x-rodionov <code512edu@gmail.com>
2021-11-28 09:05:21 +0600

    test globals 2

--- functions/hello.js
+++ functions/hello.js
@@ -1,7 +1,11 @@
 const a = {b: 'c'};
+const aa = {bb: 'cc'};
+const aaa = {bbb: 'ccc'};
 
 exports.handler = async (event, context) => {
   console.log(a);
+  eval('console.log(aa);');
+  eval('"use strict";\nconsole.log(aaa);');
   return {
     statusCode: 200,
     body: 'hello from netlify functions'
